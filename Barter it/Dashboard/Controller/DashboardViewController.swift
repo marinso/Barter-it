99377bcbdba57bd99a04afcb58847883c3c0526f
Martin Nasierowski <mnasierowski@netkodo.com>
2020-05-16 16:39:28 +0200

    Initial Commit

---
+++ Barter it/Dashboard/Controller/DashboardViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DashboardViewController.swift
+//  Barter it
+//
+//  Created by Martin Nasierowski on 16/05/2020.
+//  Copyright © 2020 Martin Nasierowski. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+    
+}
---
+++ Barter it/Login/ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Barter it
+//
+//  Created by Martin Nasierowski on 11/05/2020.
+//  Copyright © 2020 Martin Nasierowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
